dd1cecb1330d9d3c0889460383bbb84cf02d54c0
septagon <5659797+septagon@users.noreply.github.com>
2017-11-19 12:11:36 -0800

    Approaching utility.

--- DemiurgeApp/MainWindow.xaml.cs
+++ DemiurgeApp/MainWindow.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using DemiurgeLib;
-using DemiurgeLib.Common;
 using System;
 using System.Drawing;
-using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using static DemiurgeLib.Common.Utils;
 
 namespace DemiurgeApp
@@ -16,6 +15,68 @@ namespace DemiurgeApp
         public MainWindow()
         {
             InitializeComponent();
+
+            SetUpWaterTableArgsRows();
+
+            // TODO: Do the same for MSM args and all other args.
+
+            // TODO: Enable interactivity, so you can generate a WTF, then generate an MSM, then generate output, all independently.
+        }
+
+        private void SetUpWaterTableArgsRows()
+        {
+            WaterTableArgs defaultArgs = new WaterTableArgs();
+            var fields = typeof(WaterTableArgs).GetFields();
+
+            for (int idx = 0; idx < fields.Length; idx++)
+            {
+                this.SettingsGrid.RowDefinitions.Add(new RowDefinition());
+
+                // Make the label.
+                TextBlock label = new TextBlock();
+                label.Text = fields[idx].Name;
+                Grid.SetRow(label, idx);
+                Grid.SetColumn(label, 0);
+                this.SettingsGrid.Children.Add(label);
+
+                // Make the content display/setter.
+                Type type = fields[idx].FieldType;
+                if (type == typeof(int) || type == typeof(long) || type == typeof(float))
+                {
+                    TextBox value = new TextBox();
+                    value.Text = fields[idx].GetValue(defaultArgs).ToString();
+                    Grid.SetRow(value, idx);
+                    Grid.SetColumn(value, 2);
+                    this.SettingsGrid.Children.Add(value);
+                }
+                else if (type == typeof(string))
+                {
+                    CheckBox checkBox = new CheckBox();
+                    Grid.SetRow(checkBox, idx);
+                    Grid.SetColumn(checkBox, 1);
+                    this.SettingsGrid.Children.Add(checkBox);
+
+                    // TODO: Support string fields other than files.
+                    Button button = new Button();
+                    button.Content = fields[idx].GetValue(defaultArgs);
+                    button.Click += (sender, e) =>
+                    {
+                        // File picker.
+                        //var dialog = new System.Windows.Forms.OpenFileDialog();
+                        var dialog = new System.Windows.Forms.FolderBrowserDialog();
+                        var result = dialog.ShowDialog();
+                        if (result == System.Windows.Forms.DialogResult.OK)
+                        {
+                            // Do something.
+                            //button.Content = dialog.FileName;
+                            button.Content = dialog.SelectedPath;
+                        }
+                    };
+                    Grid.SetRow(button, idx);
+                    Grid.SetColumn(button, 2);
+                    this.SettingsGrid.Children.Add(button);
+                }
+            }
         }
 
         private void RunMeterScaleMapScenario(object sender, RoutedEventArgs e)
@@ -35,6 +96,11 @@ namespace DemiurgeApp
             
             msm.OutputHighLevelMaps(new Bitmap(waters.Width, waters.Height), "C:\\Users\\Justin Murray\\Desktop\\egwethoon\\");
             msm.OutputMapGrid(100, "C:\\Users\\Justin Murray\\Desktop\\egwethoon\\", "submap", 32);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
